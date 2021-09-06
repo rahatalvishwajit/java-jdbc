75906032e6963e230062dc1313ceaf29ce3ef0b6
Vishwajit Rahatal <rahatalvishwajit@gmail.com>
2021-09-06 13:01:29 +0530

    Java JDBC Initial Code

---
+++ JDBC/src/main/java/jdbc/rating/JDBCConnection.java
@@ -0,0 +1,25 @@
+package jdbc.rating;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCConnection {
+
+	public static Connection getConnection() {
+		
+		Connection conn = null;
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "12345");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error - " + e);
+		} catch (SQLException e) {
+			System.out.println("Error - " + e);
+		}
+		return conn;
+	}
+	
+}
